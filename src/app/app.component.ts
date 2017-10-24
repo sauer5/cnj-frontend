e0f7f1fba46eed7a275a85d7426fe9f6a9c35609
Sauer <hm-sauer5@hm.edu>
2017-10-24 13:53:43 +0200

    Commit vor erstem Push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+message: string = null;
+error: boolean = false;
+constructor(private http: Http) {
+}
+checkInfo() {
+this.http.get('/info').map(response => response.json()).subscribe(info => {
+this.message = info.message;
+this.error = false;
+},
+error => {
+this.error = true;
+this.message = 'ERROR';
+}
+);
+}
 }
---
+++ src/app/info.ts
@@ -0,0 +1,3 @@
+export class Info {
+	message : string;
+}
